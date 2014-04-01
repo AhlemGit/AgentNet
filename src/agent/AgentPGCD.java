9472754cd9cb77491d03b745064541eab1745284
AhlemGit <khadir.ahlem@gmail.com>
2014-04-01 01:45:02 +0100

    First_commit

---
+++ src/agent/AgentPGCD.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agent;
+
+/**
+ *
+ * @author Ahlem
+ */
+import jade.core.Agent;
+
+public class AgentPGCD extends Agent {
+	int x; int y;
+	
+	protected void setup() {
+		System.out.println("Nom de l'agent est : " + getLocalName());
+		Object[] args = getArguments();
+		if(args != null) {
+			x = Integer.valueOf(args[0].toString());
+			y = Integer.valueOf(args[1].toString());
+			while (x!=y) {
+				if(x>y) x = x-y;
+				else y = y-x;
+			}
+			
+			System.out.println("Le PGDC est : " + x);
+		}
+		doDelete();
+	}
+}
+
